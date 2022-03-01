1b52dd7f4c84b6b3e30e4147f799178d4f63839d
Erika Perciliano <erikaperciliano.developer@gmail.com>
2022-03-01 17:47:54 -0300

    calling the TemporaryShelf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,8 @@ import { useSession } from '@faststore/sdk'
 import { graphql } from 'gatsby'
 import { GatsbySeo, JsonLd } from 'gatsby-plugin-next-seo'
 import React from 'react'
-import BannerText from 'src/components/sections/BannerText'
-import Hero from 'src/components/sections/Hero'
-import IncentivesHeader from 'src/components/sections/Incentives/IncentivesHeader'
-import ProductShelf from 'src/components/sections/ProductShelf'
-import ProductTiles from 'src/components/sections/ProductTiles'
+import TemporaryShelf from 'src/components/sections/TemporaryShelf'
+import Countdown from 'src/components/sections/Countdown'
 import { mark } from 'src/sdk/tests/mark'
 import type { PageProps } from 'gatsby'
 import type { HomePageQueryQuery } from '@generated/graphql'
@@ -25,80 +22,21 @@ function Page(props: Props) {
   const siteUrl = `https://${host}${pathname}`
   const products = allStoreProduct?.nodes
 
+  const endTime = new Date().getTime() + 60000 * 1; // 2minutes
+  const [timeLeft, setEndTime] = Countdown(endTime);
+
+  const minutes = Math.floor(timeLeft / 60000) % 60;
+  const seconds = Math.floor(timeLeft / 1000) % 60;
+
   return (
     <>
-      {/* SEO */}
-      <GatsbySeo
-        title={title}
-        description={site?.siteMetadata?.description ?? ''}
-        titleTemplate={site?.siteMetadata?.titleTemplate ?? ''}
-        language={locale}
-        canonical={siteUrl}
-        openGraph={{
-          type: 'website',
-          url: siteUrl,
-          title: title ?? '',
-          description: site?.siteMetadata?.description ?? '',
-        }}
-      />
-      <JsonLd
-        json={{
-          '@context': 'https://schema.org',
-          '@type': 'WebSite',
-          url: siteUrl,
-          potentialAction: {
-            '@type': 'SearchAction',
-            target: `${siteUrl}/s/?q={search_term_string}`,
-            'query-input': 'required name=search_term_string',
-          },
-        }}
-      />
-      {/*
-        Sections: Components imported from '../components/sections' only.
-        Do not import or render components from any other folder in here.
-      */}
-      <section className="page__section">
-        <Hero
-          title="New Products Available"
-          subtitle="At FastStore you can shop the best tech of 2022. Enjoy and get 10% off on your first purchase."
-          linkText="See all"
-          link="/"
-          imageSrc="https://storeframework.vtexassets.com/arquivos/ids/190897/Photo.jpg"
-          imageAlt="Quest 2 Controller on a table"
-        />
-      </section>
-
-      <section className="page__section">
-        <IncentivesHeader />
-      </section>
-
       <section className="page__section page__section-shelf / grid-section">
-        <h2 className="title-section / grid-content">Most Wanted</h2>
         <div className="page__section-content">
-          <ProductShelf products={products?.slice(0, 5)} />
-        </div>
-      </section>
-
-      <section className="page__section / grid-section grid-content">
-        <h2 className="title-section">Just Arrived</h2>
-        <div className="page__section-content">
-          <ProductTiles products={products?.slice(5, 8)} />
-        </div>
-      </section>
-
-      <section className="page__section / grid-section">
-        <BannerText
-          title="Receive our news and promotions in advance."
-          caption="Enjoy and get 10% off on your first purchase."
-          actionPath="/"
-          actionLabel="Call to action"
-        />
-      </section>
-
-      <section className="page__section page__section-shelf / grid-section">
-        <h2 className="title-section / grid-content">Deals & Promotions</h2>
-        <div className="page__section-content">
-          <ProductShelf products={products?.slice(9, 14)} />
+          <h1 className="title-section / grid-content">{`Countdown: ${minutes}:${seconds}`}</h1>
+          {
+            seconds > 0 &&
+            <TemporaryShelf products={products?.slice(0, 5)}/>
+          }
         </div>
       </section>
     </>
